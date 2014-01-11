02f912ab6e2c602da583cf2b4e5379f5a555ae7c
Joani COUMA <joani.couma@gmail.com>
2014-01-11 11:00:07 +0100

    adding Application.java

---
+++ src/jeepractice/Application.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeepractice;
+
+import java.net.URL;
+import java.util.Date;
+
+/**
+ *
+ * @author jocouma1
+ */
+public class Application {
+
+    
+    private String name;
+    
+    private int version;
+    
+    private Date releaseDate;
+    
+    private URL website;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public int getVersion()
+    {
+        return version;
+    }
+    
+    public void setVersion(int version)
+    {
+        this.version = version;
+    }
+    
+    public Date getReleaseDate()
+    {
+        return releaseDate;
+    }
+    
+    public void setReleaseDate(Date releaseDate)
+    {
+        this.releaseDate = releaseDate;
+    }
+    
+    public URL getWebsite()
+    {
+        return website;
+    }
+    
+    public void setWebsite(URL url)
+    {
+        this.website = url;
+    }
+    
+}
