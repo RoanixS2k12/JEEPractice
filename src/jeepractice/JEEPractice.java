e12b7023af4b883c308960dafbc3fc569f1022e7
Joani COUMA <joani.couma@gmail.com>
2014-01-11 10:48:36 +0100

    Init project

---
+++ src/jeepractice/JEEPractice.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeepractice;
+
+/**
+ *
+ * @author jocouma1
+ */
+public class JEEPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
