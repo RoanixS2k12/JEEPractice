2d78fbe7c8f96178eca11212a4865ce16ff3d503
Joani COUMA <joani.couma@gmail.com>
2014-01-18 11:08:26 +0100

    Initial Creation

--- src/org/github/roanixs/jee/JEEPractice.java
+++ src/org/github/roanixs/jee/JEEPractice.java
@@ -2,7 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.github.roanixs.jee;
+package org.github.roanixs.autocreate;
+
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -14,6 +22,40 @@ public class JEEPractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("simpleAppShopPU");
+        EntityManager em = emf.createEntityManager();
+        
+        Application app = new Application();
+    
+        app.setApplicationName("Test app");
+        app.setApplicationReleaseDate(Calendar.getInstance().getTime());
+        app.setApplicationVersion("1.0.0");
+        app.setApplicationWebsite("http://google.com");
+        
+        em.getTransaction().begin();
+        try
+        {
+            em.persist(app);
+            System.out.println("Application now in persistence");
+        }
+        catch(EntityExistsException e)
+        {
+            System.out.println("App already exists");
+            em.getTransaction().rollback();
+        }
+        
+        em.getTransaction().commit();
+        
+        em.close();
+        emf.close();
+        
+        
+        
+        
+    
     }
+    
+    
 }
