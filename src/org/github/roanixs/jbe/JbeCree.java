fecaf3b6c908bb2eb31b04736be2303fd47e75d3
Olivier GOYON <goyon.olivier@gmail.com>
2014-01-25 09:54:33 +0100

    ignore

---
+++ src/org/github/roanixs/jbe/JbeCree.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.github.roanixs.jbe;
+
+/**
+ *
+ * @author olgoyon
+ */
+public class JbeCree {
+    
+}
